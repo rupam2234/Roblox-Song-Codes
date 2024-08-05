fc455698fa4fac8440ce815d627e5c2dc8df1d9f
rupam2234 <rupam.bharali99@gmail.com>
2024-08-05 11:48:44 +0530

    Adsense Update

--- src/components/Sidebar-Elements/Ads.tsx
+++ src/components/Sidebar-Elements/Ads.tsx
@@ -1,17 +1,15 @@
 const Ads = () => {
   const adsenseCode = `
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7255780745640035"
-            crossorigin="anonymous"></script>
-        <!-- Roblox Tool AD -->
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-7255780745640035"
-            data-ad-slot="9680232949"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+     crossorigin="anonymous"></script>
+<!-- Roblox Site Sidebar -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:250px;height:500px"
+     data-ad-client="ca-pub-7255780745640035"
+     data-ad-slot="2144460063"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     `;
   return (
     <div className="mt-10">
