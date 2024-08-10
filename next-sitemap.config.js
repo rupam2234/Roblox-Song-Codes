8dc68498a3ce5f6614d0d014ec9126a5c7e36ea5
rupam2234 <rupam.bharali99@gmail.com>
2024-08-10 21:06:01 +0530

    Fix 2.0.9

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   siteUrl: "https://roblox.geekguidez.com", // Update this to your production URL
   generateRobotsTxt: true, // (optional) Generate a robots.txt file
-  exclude: ["/icon.ico", "/api/songs", "/api/popularTrack", "/track"],
+  exclude: ["/icon.ico", "/api/songs", "/api/popularTrack"],
 };
