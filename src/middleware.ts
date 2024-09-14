a6e0ae1bbb6ade48b3c0d1dfee06f6ea9269f519
rupam2234 <rupam.bharali99@gmail.com>
2024-09-14 22:01:52 +0530

    Domain Change | 2.2.4

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,12 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
 
+  // domain redirect
+  if (url.hostname === "roblox.geekguidez.com") {
+    url.hostname = "robloxsongcodes.com";
+    return NextResponse.redirect(url)
+  }
+
   // Check if the URL is exactly /track without query parameters
   if (url.pathname === "/track" && url.search === "") {
     url.pathname = "/popular-tracks";
