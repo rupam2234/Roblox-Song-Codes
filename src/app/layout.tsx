2af9f896844b3cac0eb1427b30f87792ad10294a
rupam2234 <rupam.bharali99@gmail.com>
2024-09-14 11:10:10 +0530

    Grow Me Script Installed | 2.2.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,14 @@ export default function RootLayout({
           name="google-site-verification"
           content="B7bk0lZ8erO_gdIJbWXsOWsUZAkd48i8wxa2ecf0SME"
         />
+        <script
+          data-grow-initializer=""
+          dangerouslySetInnerHTML={{
+            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo0MDA2ZDMxMy1lYWI2LTQzMDAtYjY3Ny0wZDA3MWI2OWZmMDY=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})()`,
+          }}
+        />
       </head>
-      <body className={cn("text-[17px]",inter.className)}>
+      <body className={cn("text-[17px]", inter.className)}>
         <Header />
         {children}
         <Analytics />
