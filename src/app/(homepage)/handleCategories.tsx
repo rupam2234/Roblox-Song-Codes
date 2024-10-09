6336cdb8c0495e52cd641521822f7fde60922266
rupam2234 <rupam.bharali99@gmail.com>
2024-10-09 20:39:06 +0530

    Fix 2.2.4 | Colour Adjustments

--- src/app/(homepage)/handleCategories.tsx
+++ src/app/(homepage)/handleCategories.tsx
@@ -54,10 +54,10 @@ const HandleCategories = () => {
       <div>
         <TableDropdownMenu tags={tags} songAPIAddress={"songs"} />
       </div>
-      <p className="text-sm text-[#FA8900] my-4">
-        <strong>Tip:</strong> if you can&apos;t find your track, try searching
-        on the site header. It searches our entire track database instead of
-        searching in a category spacific table.
+      <p className="text-sm text-gray-600 my-4">
+        <strong className="text-[#FA8900]">Tip:</strong> if you can&apos;t find
+        your track, try searching on the site header. It searches our entire
+        track database instead of searching in a category spacific table.
       </p>
     </>
   );
