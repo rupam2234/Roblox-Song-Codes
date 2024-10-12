3de15fee6377712e7dbca4dc02459fee9afce395
rupam2234 <rupam.bharali99@gmail.com>
2024-10-12 09:38:02 +0530

    Fix 2.2.4 | Adsense Adblocker Reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,11 +45,30 @@ export default function RootLayout({
           }}
         />
         <Script
+          src="https://fundingchoicesmessages.google.com/i/pub-7255780745640035?ers=1"
           strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `<script async src="https://fundingchoicesmessages.google.com/i/pub-7255780745640035?ers=1"></script><script>(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>`,
-          }}
+          async
         />
+        <Script strategy="beforeInteractive">
+          {`
+            (function() {
+              function signalGooglefcPresent() {
+                if (!window.frames['googlefcPresent']) {
+                  if (document.body) {
+                    const iframe = document.createElement('iframe');
+                    iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                    iframe.style.display = 'none';
+                    iframe.name = 'googlefcPresent';
+                    document.body.appendChild(iframe);
+                  } else {
+                    setTimeout(signalGooglefcPresent, 0);
+                  }
+                }
+              }
+              signalGooglefcPresent();
+            })();
+          `}
+        </Script>
       </head>
       <body className={cn ? cn("text-[17px]", inter.className) : "text-[17px]"}>
         <Header />
