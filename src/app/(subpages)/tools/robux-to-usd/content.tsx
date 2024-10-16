beb6f78f698a4dc2dd593ebb0eb1e4970b3ef95a
rupam2234 <rupam.bharali99@gmail.com>
2024-10-16 10:04:14 +0530

    Release 2.2.6 | Robux to USD converter

--- src/app/(subpages)/tools/robux-to-usd/content.tsx
+++ src/app/(subpages)/tools/robux-to-usd/content.tsx
@@ -4,6 +4,38 @@ import InContentAd3 from "@/components/adsense-ads/AdsenseAd3";
 import Image from "next/image";
 
 const Content = () => {
+  const usdtorobux = [
+    { usd: 1, robux: 285.71 },
+    { usd: 10, robux: 2857.14 },
+    { usd: 100, robux: 28571.43 },
+    { usd: 1000, robux: 285714.29 },
+    { usd: 2000, robux: 571428.57 },
+    { usd: 3000, robux: 857142.86 },
+    { usd: 5000, robux: 1428571.43 },
+    { usd: 10000, robux: 2857142.86 },
+    { usd: 15000, robux: 4285714.29 },
+    { usd: 30000, robux: 8571428.57 },
+    { usd: 100000, robux: 28571428.57 },
+    { usd: 1000000, robux: 285714285.71 },
+    { usd: 10000000, robux: 2857142857.14 },
+  ];
+
+  const robuxtousd = [
+    { robux: 100, usd: 0.35 },
+    { robux: 1000, usd: 3.5 },
+    { robux: 10000, usd: 35.0 },
+    { robux: 100000, usd: 350.0 },
+    { robux: 285714, usd: 1000.0 },
+    { robux: 500000, usd: 1750.0 },
+    { robux: 1000000, usd: 3500.0 },
+    { robux: 2000000, usd: 7000.0 },
+    { robux: 5000000, usd: 17500.0 },
+    { robux: 10000000, usd: 35000.0 },
+    { robux: 20000000, usd: 70000.0 },
+    { robux: 30000000, usd: 105000.0 },
+    { robux: 100000000, usd: 350000.0 },
+  ];
+
   return (
     <div className="space-y-6">
       <p>
@@ -27,7 +59,7 @@ const Content = () => {
         rate.
       </p>
       <p>
-        Below the conversion, you&apos;ll see the exchange rate, and ofcourse
+        Below the conversion, you&apos;ll see the exchange rate, and of course
         you&apos;ll have the option to convert values in both directions.
       </p>
       <h2 className="font-bold text-[20px] text-[#5F8C81]">
@@ -85,11 +117,92 @@ const Content = () => {
         <li>EUR Rate: 0.0102 EUR / Robux</li>
         <li>GBP Rate: 0.0088 GBP / Robux</li>
       </ul>
+      <h2 className="font-bold text-[20px] text-[#5F8C81]">
+        DevX Eligibility & Conversion Prerequisite
+      </h2>
+      <p>
+        To convert your Robux into USD you must participate in Roblox&apos;s
+        Developer Exchange (DevEx) program. There are requirements for your
+        Roblox account to be eligible for DevX program:
+        <ul className="terms-list space-y-3 mt-[25px]">
+          <li>You must be at least 13 years old.</li>
+          <li>You must have a verified email address on Roblox.</li>
+          <li>
+            You must have earned at least 30,000 Robux to be able to process it.
+          </li>
+          <li>
+            Your account should comply with Roblox&apos;s community standards
+            and terms.
+          </li>
+          <li>You must have a valid DevEx portal account.</li>
+          <li>You must provide valid tax documentation with Roblox.</li>
+        </ul>
+      </p>
       <InContentAd2 />
+      <h2 className="font-bold text-[20px] text-[#5F8C81]">
+        USD to Robux Conversion Table
+      </h2>
+      <p>
+        This table represents Robux equivalent to USD for various amount of USD
+        according to DevX current conversion rate:
+      </p>
+      <div className="overflow-x-auto">
+        <table className="table-auto border border-gray-400 border-collapse w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 border border-gray-400">USD Amount</th>
+              <th className="px-4 py-2 border border-gray-400">
+                Robux Equivalent
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {usdtorobux.map((row, index) => (
+              <tr key={index}>
+                <td className="px-4 py-2 border border-gray-400">
+                  ${row.usd.toLocaleString()}
+                </td>
+                <td className="px-4 py-2 border border-gray-400">
+                  {row.robux.toLocaleString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <h2 className="font-bold text-[20px] text-[#5F8C81]">
+        Robux to USD Conversion Table
+      </h2>
+      <p>This table is for reverse conversion rates: Robux to USD</p>
+      <div className="overflow-x-auto">
+        <table className="table-auto w-full border border-gray-400 border-collapse">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 border border-gray-400">Robux Amount</th>
+              <th className="px-4 py-2 border border-gray-400">
+                USD Equivalent
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {robuxtousd.map((row, index) => (
+              <tr key={index}>
+                <td className="px-4 py-2 border border-gray-400">
+                  {row.robux.toLocaleString()}
+                </td>
+                <td className="px-4 py-2 border border-gray-400">
+                  ${row.usd.toLocaleString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <InContentAd3 />
       <h2 className="font-bold text-[20px] text-[#5F8C81]">What Can Change?</h2>
       <p>
         Exchange rates can fluctuate over time due to various factors, and as a
-        result, converiosn results will be different.
+        result, conversions results will be different.
       </p>
       <p>
         When the exchange rates changes updates to this calculator may not be
