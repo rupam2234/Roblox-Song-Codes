7fa149d406962f7fcc8e2ff5efdc0cfb4e1acd0d
rupam2234 <rupam.bharali99@gmail.com>
2024-10-19 08:02:29 +0530

    Fix 2.3.0 | SEO fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
   siteUrl: "https://robloxsongcodes.com/", // Update this to your production URL
-  generateRobotsTxt: true, // (optional) Generate a robots.txt file
-  exclude: ["/icon.ico", "/api/songs", "/api/popularTrack"],
+  generateRobotsTxt: false, // (optional) Generate a robots.txt file
+  exclude: ["/icon.ico", "/api/songs", "/api/popularTrack", "/api/", "/_next/"],
 };
