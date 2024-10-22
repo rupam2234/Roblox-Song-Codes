f641bcbbb69cf43a5fb7c3bde5698962d5257666
rupam2234 <rupam.bharali99@gmail.com>
2024-10-22 17:59:49 +0530

    Fix 2.3.0 | Adsense Ad Test Sidebar

--- src/components/Sidebar-Elements/Ads.tsx
+++ src/components/Sidebar-Elements/Ads.tsx
@@ -4,15 +4,17 @@ const Ads = () => {
      crossorigin="anonymous"></script>
 <!-- Roblox Song Codes Top Sidebar -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:300px"
+     style="display:block"
      data-ad-client="ca-pub-7255780745640035"
-     data-ad-slot="5672722054"></ins>
+     data-ad-slot="5672722054"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
     `;
   return (
-    <div className="mx-[15px] text-center">
+    <div className="mx-[15px] w-auto h-[300px] text-center">
       <div dangerouslySetInnerHTML={{ __html: adsenseCode }} />
     </div>
   );
