27f2d09f0527c0f9817ba5728e07299c160bb5c9
rupam2234 <rupam.bharali99@gmail.com>
2024-11-12 12:29:11 +0530

    Fix 2.3.2 | Title Update

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -11,7 +11,7 @@ const month = date.toLocaleString("default", { month: "long" });
 const year = date.getFullYear();
 
 export const metadata: Metadata = {
-  title: `Roblox Song Codes: Best Roblox Music IDs (${month} - ${year})`,
+  title: `Best Roblox Song Codes & Music IDs (${month} - ${year})`,
   description:
     "This is a collection of Roblox song codes that are currently working. The database is consistently updated to with the latest public Song IDs from creator's hub.",
   keywords: [
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   ],
   publisher: "RobloxSongCodes.com",
   openGraph: {
-    title: `Roblox Song Codes: Best Roblox Music IDs (${month} - ${year})`,
+    title: `Best Roblox Song Codes & Music IDs (${month} - ${year})`,
     description:
       "This is a collection of Roblox song codes that are currently working. The database is consistently updated to with the latest public Song IDs from creator's hub.",
     siteName: "RobloxSongCodes.com",
@@ -39,15 +39,13 @@ const HomePage = () => {
       <div className="grid grid-cols-12 md:gap-6 min-h-screen lg:p-2 px-6 mt-8 lg:px-[170px]">
         <div className="lg:col-span-8 col-span-12 flex-col items-center">
           <HomapageSchema
-            Headline={`Roblox Song Codes: Groove With the Best Roblox Music IDs`}
+            Headline={`Best Roblox Song Codes & Music IDs (${month} - ${year})`}
             PageUrl={"/"}
             PageAbout={
               "This is a complete list of all tested Roblox song codes across different genres that you can play on Boombox. We consistently update and filter this database to provide you with song IDs that work."
             }
             AuthorName={"Rupam Bharali"}
-            PageName={
-              "Roblox Song Codes: Groove With the Best Roblox Music IDs"
-            }
+            PageName={`Best Roblox Song Codes & Music IDs (${month} - ${year})`}
           />
           <TopContent />
           <HandleCategories />
